35f06e7748219c2ee157acd552196fc1af34b107
kaanboraoz <kaansdk@pm.me>
2026-01-22 14:25:32 +0300

    chore: remove outdated comment from main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod ui;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
-    ratatui::run(app)?; // Burada app fonksiyonunu bir değer olarak gönderiyoruz fonksiyonu call eden ve argüman gönderne run fonksiyonudur
+    ratatui::run(app)?; 
     Ok(())
 }
